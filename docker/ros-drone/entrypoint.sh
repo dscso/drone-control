e16cc1553f789a6630d2b9206f6d99b3661b6a4e
Jurek Weber <git@jurek.io>
2023-01-10 09:13:23 +0100

    try again windows

--- docker/ros-drone/entrypoint.sh
+++ docker/ros-drone/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export ROS1_INSTALL_PATH=/opt/ros/melodic
 
+export ROS1_INSTALL_PATH=/opt/ros/melodic
 ROS_DISTRO=melodic source ${ROS1_INSTALL_PATH}/setup.bash
 source /root/bebop_ws/devel/setup.bash
 export LD_LIBRARY_PATH=/opt/ros/melodic/lib/parrot_arsdk:/opt/ros/melodic/lib/:$LD_LIBRARY_PATH
