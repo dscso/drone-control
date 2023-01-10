dfa69c309dce6dc12b8305dc43268fe320bad6fe
Jurek Weber <git@jurek.io>
2023-01-10 09:09:24 +0100

    changing from CR o LF

--- docker/ros-drone/entrypoint.sh
+++ docker/ros-drone/entrypoint.sh
@@ -5,5 +5,5 @@ ROS_DISTRO=melodic source ${ROS1_INSTALL_PATH}/setup.bash
 source /root/bebop_ws/devel/setup.bash
 export LD_LIBRARY_PATH=/opt/ros/melodic/lib/parrot_arsdk:/opt/ros/melodic/lib/:$LD_LIBRARY_PATH
 export CATKIN_WS="~/catkin_ws"
-sleep 3
+
 exec "$@"
