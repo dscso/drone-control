eac5eb8ec5bfb6d26700c4de164a451e0f111866
Jurek Weber <git@jurek.io>
2023-01-13 08:57:15 +0100

    creating base infrastructure with roscore and drone connector isolation

--- docker/ros-drone/entrypoint.sh
+++ docker/ros-drone/entrypoint.sh
@@ -5,5 +5,6 @@ ROS_DISTRO=melodic source ${ROS1_INSTALL_PATH}/setup.bash
 source /root/bebop_ws/devel/setup.bash
 export LD_LIBRARY_PATH=/opt/ros/melodic/lib/parrot_arsdk:/opt/ros/melodic/lib/:$LD_LIBRARY_PATH
 export CATKIN_WS="~/catkin_ws"
+export PYTHONUNBUFFERED=1 # supervisord
 
 exec "$@"
