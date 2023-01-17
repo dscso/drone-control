e612656332a5c72c5f8ad20550109024fb5ca3eb
Jurek <git@jurek.io>
2023-01-17 09:55:11 +0100

    removing ros 2

--- docker/ros-bridge/rosbridge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export ROS2_INSTALL_PATH=/opt/ros/foxy
-export ROS2_PATH=/opt/ros/foxy
-
-ROS_DISTRO=foxy source ${ROS2_INSTALL_PATH}/setup.bash
-export ROS_MASTER_URI=http://localhost:11311
-
-exec "$@"
--- docker/ros-bridge/roscore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export ROS1_INSTALL_PATH=/opt/ros/noetic
-export ROS1_PATH=/opt/ros/noetic
-
-ROS_DISTRO=noetic source ${ROS1_INSTALL_PATH}/setup.bash
-
-roscore
